46a4408e78de45e0bf868ac133795af80399e927
raksha138 <113582403+raksha138@users.noreply.github.com>
2024-08-06 22:54:30 +0530

    Updated Footer

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
 
                 <Grid item xs={12} sm={6} md={3}>
 
-                    <Typography className='pb-5' variant='h6'>Solution</Typography>
+                    <Typography className='pb-5' variant='h6'>Solutions</Typography>
                     <div>
                         <Button className='pb-5' variant='h6' gutterBottom>Marketing</Button>
                     </div>
